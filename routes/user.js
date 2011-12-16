77b7a3e1a76ef7dbfcc7aabf28ad0053d4d30cb7
Aditya Bhatt <aditya@adityabhatt.org>
2011-12-16 23:39:19 +0530

    minor changes

--- routes/user.js
+++ routes/user.js
@@ -5,21 +5,24 @@ app.get('/:username', function(req, res) {
             turbulence.queryAgent('agent_person', {username: req.params.username}, function(err, user) {
                 if(err) console.log(err);
                 else {
-                    var search_regex = new RegExp('@' + req.params.username, 'i');
-                    var howls_per_page = 25;
-                    turbulence.queryPost(   
-                        {$or: [{username: req.params.username}, {text: search_regex}] },
-                        {sort: [['date', 'descending']], limit: howls_per_page},
-                        function(err, howls) {
-                            if(err) console.log(err.message);
-                            else {
-                                res.local('user', user);
-                                res.local('howls', howls);
-                                res.local('account', account);
-                                res.local('title', 'Howler - ' + req.params.username);
-                                res.render('user');
-                            }
-                    });
+                    if(!user)
+                        console.log('could not find user');
+                    else {
+                        var howls_per_page = 25;
+                        turbulence.queryPost(
+                            {authorAgent: user._id},
+                            {sort: [['date', 'descending']], limit: howls_per_page},
+                            function(err, howls) {
+                                if(err) console.log(err.message);
+                                else {
+                                    res.local('user', user);
+                                    res.local('howls', howls);
+                                    res.local('account', account);
+                                    res.local('title', 'Howler - ' + req.params.username);
+                                    res.render('user');
+                                }
+                        });
+                    }
                 }
             });
         }
