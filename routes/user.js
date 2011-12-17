f040562cd99183d076daee7205d9355dcc5084c7
Aditya Bhatt <aditya@adityabhatt.org>
2011-12-17 09:09:19 +0530

    log user howls

--- routes/user.js
+++ routes/user.js
@@ -15,6 +15,7 @@ app.get('/:username', function(req, res) {
                             function(err, howls) {
                                 if(err) console.log(err.message);
                                 else {
+                                    console.log(howls);
                                     res.local('user', user);
                                     res.local('howls', howls);
                                     res.local('account', account);
