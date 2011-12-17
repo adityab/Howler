696ff87a7f7383c47270c20dcab1578628ec6b32
Aditya Bhatt <aditya@adityabhatt.org>
2011-12-17 19:46:42 +0530

    Beauty + about page

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app = module.exports = express.createServer('127.0.0.1');
 
 // Connect to the Turbulence engine
 turbulence = require('turbulence-engine')
-turbulence.connect('localhost', 'howler_db');
+turbulence.connect('localhost', 'newdb');
 
 connect = require('connect');
 auth = require('connect-auth');
--- routes/about.js
+++ routes/about.js
@@ -1,4 +1,10 @@
 app.get('/about/this', function(req, res) {
-    res.local('title', 'About Howler');
-    res.render('about');
+    loadAccount(req, function(err, account) {
+        if(err) console.log(err);
+        else {
+            res.local('account', account);
+            res.local('title', 'About Howler');
+            res.render('about');
+        }
+    });
 });
